ddebe78e7653677dc44cb36199b353928eb0f9bf
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-02-20 19:32:06 +0700

    v24

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,8 +6,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Tab } from 'bootstrap';
-
+import { useSelector } from 'react-redux';
 const Header = (props) => {
+    const listUser = useSelector(state => state.user.listUser);
+
+
     return (
 
         <Navbar collapseOnSelect expand="lg" bg='light' className="bg-body-tertiary">
@@ -16,18 +19,13 @@ const Header = (props) => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown style={{ marginLeft: '50px' }} title={`${listUser.length}`} id="collapsible-nav-dropdown">
+                            {listUser && listUser.length > 0 && listUser.map((item, index) => {
+                                return (
+                                    <NavDropdown.Item key={`user - ${index}`} href="#action/3.1">{item.email}</NavDropdown.Item>
+                                )
+                            })}
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Nav>
